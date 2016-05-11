349d0c16b4ea565a7bcdd457e10e9fbf1f332c02
Mueller <UC195262@ten.thomsonreuters.com>
2016-05-11 11:00:10 -0500

    Update to latest

--- src/client/app/components/app.component.ts
+++ src/client/app/components/app.component.ts
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 import {Component} from '@angular/core';
-=======
-import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES, Routes } from '@angular/router';
-
-import { AboutComponent } from '../+about/index';
-import { HomeComponent } from '../+home/index';
-import { NameListService } from '../shared/index';
-import { NavbarComponent } from './navbar.component';
-import { ToolbarComponent } from './toolbar.component';
->>>>>>> f42298817400d7bde57019b4536e05621b9af823
 
 @Component({
   selector: 'sd-app',
