73f207c6391d7a8d2420eabd2d18c19ae072a120
Mueller <UC195262@ten.thomsonreuters.com>
2016-06-03 11:42:50 -0500

    Adding more comments.

--- src/client/app/main.ts
+++ src/client/app/main.ts
@@ -11,24 +11,25 @@ import {CMBootstrapper} from './components/editor/index';
 import {WidgetComponentFactory} from './workbench/widgetComponentFactory';
 
 if ('<%= ENV %>' === 'prod') { enableProdMode(); }
-// Bootstrap CodeMirror
-//CMBootstrapper.load();
 
 var platform = createPlatform(ReflectiveInjector.resolveAndCreate([BROWSER_PROVIDERS]));
 var appInjector = ReflectiveInjector.resolveAndCreate([BROWSER_APP_DYNAMIC_PROVIDERS, WidgetComponentFactory, provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' })], platform.injector);
 
-// Load widgetComponentContainer classes used by WorkBench application.
-(<any>window).jwAngular = {};
-DependencyLoader.load((<any>window).jwAngular);
-
-// TODO: Add this to JW params so it can be injected to WidgetComponentContainer's.
-var widgetComponentFactory: WidgetComponentFactory = appInjector.get(WidgetComponentFactory);
-(<any>window).widgetComponentFactory = widgetComponentFactory;
-
+// Bootstrap NG2
 coreLoadAndBootstrap(appInjector, AppComponent).then((componentRef: ComponentRef<AppComponent>) => {
-  // Bootstrap WorkBench.
+  /*** START - BOOTSTRAP WORKBENCH ***/
+  // TODO: Add this to JW params so it can be injected to WidgetComponentContainer's.
+  var widgetComponentFactory: WidgetComponentFactory = appInjector.get(WidgetComponentFactory);
+  (<any>window).widgetComponentFactory = widgetComponentFactory;
+  // Load widgetComponentContainer classes used by WorkBench application.
+  (<any>window).jwAngular = {};
+  DependencyLoader.load((<any>window).jwAngular);
+  // Load CodeMirror
+  //CMBootstrapper.load();
+  // Launch WorkBench.
   var main = document.getElementById('testMain');
   (<any>window).executeLoadAndLaunchChain(main);
+  /*** STOP - BOOTSTRAP WORKBENCH ***/
 })
 
 // In order to start the Service Worker located at "./worker.js"
